d58b21209f146caab9ae8e89e2554c6b9040fa9c
Lochan-Vishwanath <33321391+Lochan-Vishwanath@users.noreply.github.com>
2019-10-31 10:16:14 +0530

    Cleaning and Organizing Project.

---
+++ Assets/SCRIPTS/SoundandMusic.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundandMusic : MonoBehaviour
+{
+    public AudioClip WIN,FAIL,Knockback;
+
+    public static bool SWIN, SFAIL, SKnockback;
+
+    AudioSource As;
+
+    private void Start()
+    {
+        As = GetComponent<AudioSource>();
+        SWIN = SFAIL = SKnockback = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            SKnockback = true;
+        }
+        if (collision.gameObject.tag == "Good")
+        {
+            SWIN = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (SWIN)
+        {
+            As.PlayOneShot(WIN);
+            SWIN = false;
+        }
+        if (SFAIL)
+        {
+            As.PlayOneShot(FAIL);
+            SFAIL = false;
+        }
+        if (SKnockback)
+        {
+            As.PlayOneShot(Knockback);
+            SKnockback = false;
+        }
+    }
+}
